ae7859d6d020d48fe7b9a70b738a81e34fc99806
Keith Carlos <108279070+Kratos3434@users.noreply.github.com>
2024-03-02 02:55:23 -0500

    Update socket.ts

--- src/socket.ts
+++ src/socket.ts
@@ -1,5 +1,5 @@
 import { io } from 'socket.io-client';
 
-export const socket = io("http://ec2-3-96-216-225.ca-central-1.compute.amazonaws.com", {
+export const socket = io("https://ec2-3-96-216-225.ca-central-1.compute.amazonaws.com", {
   autoConnect: false
 });
